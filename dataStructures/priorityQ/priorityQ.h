8fc6d610a041e9aa9e4f357204e8f6ce4cd620b1
Ashton Vang <ashtonvang001@Ashtons-MacBook-Air.local>
2024-04-02 10:02:05 -0700

    priorityQ.h

--- dataStructures/priorityQ/priorityQ.h
+++ dataStructures/priorityQ/priorityQ.h
@@ -1,13 +1,42 @@
 #ifndef PRIORITYQ_H
 #define PRIORITYQ_H
 
-class PriorityQ {
-    public:
-        PriorityQ();
-        ~PriorityQ();
+class PriorityQueue{
+private:
+    int capacity;
+    int size;
+    int* a;
 
-    private:
-        
+public:
+
+    PriorityQueue(){
+        capacity = 0;
+        size = 0;
+        a = nullptr;
+    }
+    PriorityQueue(int cap){
+        capacity = cap;
+        size = 0;
+        a = new int [capacity];
+    }
+
+    ~PriorityQueue(){
+        delete[] a;
+    }
+
+    int getParent(int i);
+    int getLeftChild(int i);
+    int getRightChild(int i);
+    void heapDown(int i);
+    void heapUp(int i);
+    void insert(int x);
+    int extractMax();
+    void changeP(int i, int x);
+    int getMax();
+    void delete1(int x);
+    void display();
+
+
 };
 
 #endif
