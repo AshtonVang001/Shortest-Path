f07784716052bab58c7f7fed60bd32b0bd839f1e
Nick Davis <ndavis55862@gmail.com>
2024-04-13 16:41:53 -0700

    make map 2 and update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,21 @@
 using namespace std;
 
 int main() {
-    int mapSize = 25; //mapSize must be a perfect square
-    Array2d map(mapSize);
-    map = makeMap("maps/map1.txt");
-    map.PrintArray2d();
-    Graph graph(mapSize);
-    graph.MakeGraphFromMap(map);
-    graph.matrix.PrintArray2d();
+    int map1Size = 25; //mapSize must be a perfect square
+    Array2d map1(map1Size);
+    map1 = makeMap("maps/map1.txt");
+    map1.PrintArray2d();
+    Graph graph1(map1Size);
+    graph1.MakeGraphFromMap(map1);
+    graph1.matrix.PrintArray2d();
+
+    int map2Size = 36; //mapSize must be a perfect square
+    Array2d map2(map2Size);
+    map2 = makeMap("maps/map2.txt");
+    map2.PrintArray2d();
+    Graph graph2(map2Size);
+    graph2.MakeGraphFromMap(map2);
+    graph2.matrix.PrintArray2d();
 
     return 0;
 }
